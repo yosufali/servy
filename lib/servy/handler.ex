3be94c12aca5e4f2a81a8f49b4098eabec81a2bd
Yosuf Ali <yosufali@users.noreply.github.com>
2019-11-19 19:42:39 +0000

    Refactor rewrite_path to use regexes.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -10,15 +10,17 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
   end
 
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-    %{conv | path: "/bears/#{id}"}
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{conv | path: "/#{thing}/#{id}"}
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path_captures(conv, nil), do: conv
 
   def log(conv), do: IO.inspect(conv)
 
