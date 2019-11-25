9fdb97617d41660b4c91b411b70a828161ddb62d
Yosuf Ali <yosufali@users.noreply.github.com>
2019-11-25 13:07:01 +0000

    Serving static files

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -73,8 +73,27 @@ defmodule Servy.Handler do
     %{conv | status: 403, resp_body: "Deleting a bear is forbidden"}
   end
 
+  def route(%{method: "GET", path: "/about"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  def handle_file({:ok, contents}, conv) do
+    %{conv | status: 200, resp_body: contents}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error #{reason}"}
   end
 
   def format_response(conv) do
@@ -161,6 +180,18 @@ IO.puts(response)
 
 request = """
 GET /bears?id=3 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
