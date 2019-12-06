ff47f78c1e95b747b25573d9f4fbdbf238255a4e
Yosuf Ali <yosufali@users.noreply.github.com>
2019-12-06 17:28:03 +0000

    Modelling with structs

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,34 +1,36 @@
 defmodule Servy.Plugins do
   require Logger
 
-  def rewrite_path(%{path: path} = conv) do
+  alias Servy.Conv
+
+  def rewrite_path(%Conv{path: path} = conv) do
     regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
     captures = Regex.named_captures(regex, path)
     rewrite_path_captures(conv, captures)
   end
 
-  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+  def rewrite_path_captures(%Conv{} = conv, %{"thing" => thing, "id" => id}) do
     %{conv | path: "/#{thing}/#{id}"}
   end
 
   def rewrite_path_captures(conv, nil), do: conv
 
   @doc "Logs requests"
-  def log(conv) do
+  def log(%Conv{} = conv) do
     Logger.info("Logging request: #{inspect(conv)}")
     conv
   end
 
-  def track(%{status: 404, path: path} = conv) do
+  def track(%Conv{status: 404, path: path} = conv) do
     IO.puts("Warning: #{path} is on the loose!")
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def emojify(%{status: 200} = conv) do
+  def emojify(%Conv{status: 200} = conv) do
     %{conv | resp_body: "🥳 \n" <> conv.resp_body <> "\n🥳"}
   end
 
-  def emojify(conv), do: conv
+  def emojify(%Conv{} = conv), do: conv
 end
