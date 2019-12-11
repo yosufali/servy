46f6df1c386fac6a3646dcf33615090547655ed0
Yosuf Ali <yosufali@users.noreply.github.com>
2019-12-11 17:00:29 +0000

    Stateful Server Processes Part 2

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,16 +1,34 @@
 defmodule Servy.PledgeServer do
-  def create_pledge(name, amount) do
-    {:ok, id} = send_pledge_to_service(name, amount)
+  def listen_loop(state) do
+    IO.puts("\n Waiting for a message")
 
-    # cache pledge
-    [{"larry", 10}]
+    receive do
+      {:create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        new_state = [{name, amount} | state]
+        IO.puts("#{name} pleadged #{amount}!")
+        IO.puts("New state is #{inspect(new_state)}")
+        listen_loop(new_state)
+
+      {sender, :recent_pledges} ->
+        send(sender, {:response, state})
+        IO.puts("Sent pledges to #{inspect(sender)}")
+        listen_loop(state)
+    end
   end
 
-  def recent_pledges do
-    # return most recent 3 pledges
+  # def create_pledge(name, amount) do
+  # {:ok, id} = send_pledge_to_service(name, amount)
 
-    [{"larry", 10}]
-  end
+  #   # cache pledge
+  #   [{"larry", 10}]
+  # end
+
+  # def recent_pledges do
+  #   # return most recent 3 pledges
+
+  #   [{"larry", 10}]
+  # end
 
   defp send_pledge_to_service(_name, _amount) do
     # code to send to pledge service
