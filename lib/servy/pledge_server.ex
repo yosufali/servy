e3534bce083b7e443436bc5d7aa4f11a0e4e3131
Yosuf Ali <yosufali@users.noreply.github.com>
2019-12-11 17:36:59 +0000

    Stateful Server Processes Part 3

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,34 +1,44 @@
 defmodule Servy.PledgeServer do
-  def listen_loop(state) do
-    IO.puts("\n Waiting for a message")
+  @process_name :pledge_server
 
+  def start do
+    IO.puts("Starting the Pledge Server...")
+
+    pid = spawn(__MODULE__, :listen_loop, [[]])
+    Process.register(pid, @process_name)
+    pid
+  end
+
+  def listen_loop(state) do
     receive do
-      {:create_pledge, name, amount} ->
+      {sender, :create_pledge, name, amount} ->
         {:ok, id} = send_pledge_to_service(name, amount)
-        new_state = [{name, amount} | state]
-        IO.puts("#{name} pleadged #{amount}!")
-        IO.puts("New state is #{inspect(new_state)}")
+        most_recent_pledges = Enum.take(state, 2)
+        new_state = [{name, amount} | most_recent_pledges]
+        send(sender, {:response, id})
         listen_loop(new_state)
 
       {sender, :recent_pledges} ->
         send(sender, {:response, state})
-        IO.puts("Sent pledges to #{inspect(sender)}")
         listen_loop(state)
     end
   end
 
-  # def create_pledge(name, amount) do
-  # {:ok, id} = send_pledge_to_service(name, amount)
+  def create_pledge(name, amount) do
+    send(@process_name, {self(), :create_pledge, name, amount})
 
-  #   # cache pledge
-  #   [{"larry", 10}]
-  # end
+    receive do
+      {:response, pledge_id} -> pledge_id
+    end
+  end
 
-  # def recent_pledges do
-  #   # return most recent 3 pledges
+  def recent_pledges do
+    send(@process_name, {self(), :recent_pledges})
 
-  #   [{"larry", 10}]
-  # end
+    receive do
+      {:response, pledges} -> pledges
+    end
+  end
 
   defp send_pledge_to_service(_name, _amount) do
     # code to send to pledge service
@@ -36,3 +46,15 @@ defmodule Servy.PledgeServer do
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
 end
+
+# alias Servy.PledgeServer
+
+# pid = PledgeServer.start()
+
+# IO.inspect(PledgeServer.create_pledge("larry", 10))
+# IO.inspect(PledgeServer.create_pledge("Mo", 20))
+# IO.inspect(PledgeServer.create_pledge("curly", 30))
+# IO.inspect(PledgeServer.create_pledge("daist", 40))
+# IO.inspect(PledgeServer.create_pledge("grace", 50))
+
+# IO.inspect(PledgeServer.recent_pledges())
