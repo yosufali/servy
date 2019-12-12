2a790d3acc135b3b6fb6537810bde1a6e5c42198
Yosuf Ali <yosufali@users.noreply.github.com>
2019-12-12 12:40:40 +0000

    Fault Recovery with OTP Supervisors Part 2

--- lib/servy/supervisor.ex
+++ lib/servy/supervisor.ex
@@ -1,16 +1,16 @@
-defmodule Servy.ServicesSupervisor do
+defmodule Servy.Supervisor do
   use Supervisor
 
-  def start_link(_arg) do
-    IO.puts("Starting the Services supervisor")
+  def start_link do
+    IO.puts("Starting the Supervisor")
 
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
     children = [
-      Servy.PledgeServer,
-      {Servy.SensorServer, 60}
+      Servy.KickStarter,
+      Servy.ServicesSupervisor
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
